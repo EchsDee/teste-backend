527087b6e3237a8ff5239a63f1688d38506f3d8b
EchsDee <61986239+EchsDee@users.noreply.github.com>
2024-03-28 11:03:21 -0300

    o Migrate do pipeline estava incorreto atualizado com o correto.

--- database/migrations/2024_03_21_170623_create_pipelines_table.php
+++ database/migrations/2024_03_21_170623_create_pipelines_table.php
@@ -9,11 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('pipelines', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->unsignedBigInteger('previous_pipeline_id')->nullable();
+            $table->unsignedBigInteger('next_pipeline_id')->nullable();
             $table->timestamps();
+            
+            $table->foreign('previous_pipeline_id')->references('id')->on('pipelines')->onDelete('set null');
+            $table->foreign('next_pipeline_id')->references('id')->on('pipelines')->onDelete('set null');
         });
     }
 
